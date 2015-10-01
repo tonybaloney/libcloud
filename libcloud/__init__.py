264e3937aa4e90f8944f2b485ef52db12a9a6254
Peter Amstutz <peter.amstutz@curoverse.com>
2015-10-01 10:08:49 -0400

    Update version

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.18.1.dev3'
+__version__ = '0.18.1.dev4'
 
 import os
 import codecs
