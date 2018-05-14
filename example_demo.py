1c99aaa0f4b425aa320db9510da3ca90a7987fd7
Anthony Shaw <anthonyshaw@apache.org>
2018-05-14 12:49:59 -0400

    add metaclass

--- example_demo.py
+++ example_demo.py
@@ -2,4 +2,9 @@ from libcloud3.drivers.demo import DemoDriver
 
 driver = DemoDriver(1234)
 
-print(driver.ComputeInstance.get())
+instances = driver.ComputeInstance.get()
+
+print(instances)
+
+print(instances[0].stop())
+
